6658205fdb61b7bdb0613f0bdca8786a7c57404d
Tomasz Dysinski <oz@bunnybones.com>
2019-01-29 08:53:57 -0800

    improved rendering speed by preventing rebinding of identical attribute buffers, if for instance you reuse some buffers across objects, like duplicated meshes whose only difference are the UV buffer

--- src/renderers/WebGLRenderer.js
+++ src/renderers/WebGLRenderer.js
@@ -709,6 +709,12 @@ function WebGLRenderer( parameters ) {
 
 	};
 
+	var boundBufferCache = []
+	for(var i = 0; i < 64; i++) {
+		boundBufferCache[i] = null
+	}
+	var boundIndexBuffer = null;
+
 	this.renderBufferDirect = function ( camera, fog, geometry, material, object, group ) {
 
 		var frontFaceCW = ( object.isMesh && object.matrixWorld.determinant() < 0 );
@@ -775,9 +781,10 @@ function WebGLRenderer( parameters ) {
 
 			setupVertexAttributes( object, geometry, material, program );
 
-			if ( index !== null ) {
+			if ( index !== null && boundIndexBuffer !== attribute.buffer) {
 
 				_gl.bindBuffer( _gl.ELEMENT_ARRAY_BUFFER, attribute.buffer );
+				boundIndexBuffer = attribute.buffer;
 
 			}
 
@@ -948,8 +955,11 @@ function WebGLRenderer( parameters ) {
 
 						}
 
-						_gl.bindBuffer( _gl.ARRAY_BUFFER, buffer );
-						_gl.vertexAttribPointer( programAttribute, size, type, normalized, stride * bytesPerElement, offset * bytesPerElement );
+						if(boundBufferCache[programAttribute] !== buffer) {
+							_gl.bindBuffer( 34962, buffer );
+							_gl.vertexAttribPointer( programAttribute, size, type, normalized, stride * bytesPerElement, offset * bytesPerElement );
+							boundBufferCache[programAttribute] = buffer;
+						}
 
 					} else {
 
@@ -969,8 +979,11 @@ function WebGLRenderer( parameters ) {
 
 						}
 
-						_gl.bindBuffer( _gl.ARRAY_BUFFER, buffer );
-						_gl.vertexAttribPointer( programAttribute, size, type, normalized, 0, 0 );
+						if(boundBufferCache[programAttribute] !== buffer) {
+							_gl.bindBuffer( 34962, buffer );
+							_gl.vertexAttribPointer( programAttribute, size, type, normalized, 0, 0 );
+							boundBufferCache[programAttribute] = buffer;
+						}
 
 					}
 
